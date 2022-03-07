00bd770e5c190d1bce56d16b35b3cffbcc70dbe3
mehovic555 <pawel.miechowicz55@wp.pl>
2022-03-07 21:31:05 +0100

    2

--- kas.py
+++ kas.py
@@ -180,6 +180,32 @@ def BlackJack(cash):
 
 
 # Ruletka
+parzyste=[0,2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36]
+nieparzyste=[1,3,5,7,9,11,13,15,17,19,21,23,25,27,29,31,33,35]
+czarne=[2,4,6,8,10,11,13,15,17,20,22,24,26,28,29,31,33,35]
+czerwone=[1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]
+t5=[0,1,2,3,4]
+k1=[1,4,7,10,13,16,19,22,25,28,31,34]
+k2=[2,5,8,11,14,17,20,23,26,29,32,35]
+k3=[3,6,9,12,15,18,21,24,27,30,33,36]
+l1=[1,2,3]
+l2=[4,5,6]
+l3=[7,8,9]
+l4=[10,11,12]
+l5=[13,14,15]
+l6=[16,17,18]
+l7=[19,20,21]
+l8=[22,23,24]
+l9=[25,26,27]
+l10=[28,29,30]
+l11=[31,32,33]
+l12=[34,35,36]
+p12=[1,2,3,4,5,6,7,8,9,10,11,12]
+d12=[13,14,15,16,17,18,19,20,21,22,23,24]
+t12=[25,26,27,28,29,30,31,32,33,34,35,36]
+p18=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+d18=[19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36]
+
 def ruletka_los():
     r=random.randint(0,36)
     return r
@@ -191,25 +217,34 @@ def ruletka_obst():
     if zak1 == "kolor":
         zak2=input("jaki to ma być kolor")
     if zak1 == "zakład specjalny":
-        zakp=input("jaki to zakład?"+"\n"+"1., 2. lub 3. 12 wpisz 12, 2 numery wpisz 2,3 numery wpisz 3, 4 numery wpisz 4, 1. 5 numerów wpisz 5 "+"\n"+"kolumny, linie, parzyste, nieparzyste")
+        zakp=input("jaki to zakład?"+"\n"+"1., 2. lub 3. 12 wpisz 12, 2 numery wpisz 2,3 numery wpisz 3, 4 numery wpisz 4, 1. 5 numerów wpisz 5 "+"\n"+"kolumny, linie, parzyste, nieparzyste, 1. lub 2. 18")
         zaklady_spec(zakp)
     return zak2
 
 def zaklady_spec(zakp):
-    if zakp == "12":
-        zak2 = input("która to ma być 12")
-    if zakp == "2":
-        zak2 = input("które to mają byc liczby")
-    if zakp == "3":
-        zak2 = input("które to mają być liczby")
-    if zakp == "4":
-        zak2 = input("które to mają być liczby")
-    if zakp == "5":
-        zak2 = [0, 1, 2, 3, 4]
+    global zak2
+    if zakp==12:
+        zak2=input("która to 12 wpisz 12 i swoją liczbę")
+    if zakp == 18:
+        zak2 = input("która to 18 wpisz 18 i swoją liczbę")
+    if zakp == 2:
+        zakp2 = input("które to liczvy wpisz 22 i swoją liczbę")
+        for i in range(2):
+            zak2.append(zakp2)
+    if zakp == 3:
+        zakp2 = input("które to liczvy wpisz 33 i swoją liczbę")
+        for i in range(3):
+            zak2.append(zakp2)
+    if zakp == 4:
+        zakp2 = input("które to liczvy wpisz 44 i swoją liczbę")
+        for i in range(4):
+            zak2.append(zakp2)
+    if zakp == 5:
+        zak2=5
     if zakp == "kolumny":
-        zak2 = input("która to ma być kolumna")
+        zak2 = input("która to ma być kolumna napisz 13 i swoją liczbę ")
     if zakp == "linie":
-        zak2 = input("która to ma byc linia")
+        zak2 = input("która to ma byc linia napisz 144 i do tego dodaj swoją linię")
     if zakp == "parzyste":
         zak2="parzyste"
     if zakp == "nieparzyste":
@@ -219,8 +254,183 @@ def zaklady_spec(zakp):
     return zak2
 
 def rsprawdzenie(zak2, r):
-    if zak2==0:
-        print()
+    global win
+    z=0
+    if zak2>=0 and zak2<=36:
+        if zak2==r:
+            win=35
+    if zak2==str:
+        if zak2=="parzyste":
+            for i in range(36):
+               if parzyste==r:
+                   z+=1
+            if z==1:
+                win=1
+        elif zak2 =="nieparzyste":
+            for i in range(36):
+                if nieparzyste==r:
+                    z+=1
+            if z==1:
+                win=1
+        elif zak2=="czerwone":
+            for i in range(18):
+                if czerwone==r:
+                    z+=1
+            if z==1:
+                win=1
+        elif zak2=="czarne":
+            for i in range(18):
+                if czarne==r:
+                    z+=1
+            if z==1:
+                win=1
+    if zak2>130 and zak2<140:#kolumna 2:1
+        a=130-int(zak2)
+        if a==1:
+            for i in range(12):
+                if k1==r:
+                    z+=1
+            if z==1:
+                win=2
+        if a==2:
+            for i in range(12):
+                if k2==r:
+                    z+=1
+            if z==1:
+                win=2
+        if a==3:
+            for i in range(12):
+                if k3==r:
+                    z+=1
+            if z==1:
+                win=2
+
+    if zak2>1440 and zak2<1460:#linia 5:1
+        a=1440-int(zak2)
+        if a==1:
+            for i in range(3):
+                if l1==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==2:
+            for i in range(3):
+                if l2==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==3:
+            for i in range(3):
+                if l3==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==4:
+            for i in range(3):
+                if l4==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==5:
+            for i in range(3):
+                if l5==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==6:
+            for i in range(3):
+                if l6==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==7:
+            for i in range(3):
+                if l7==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==8:
+            for i in range(3):
+                if l8==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==9:
+            for i in range(3):
+                if l9==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==10:
+            for i in range(3):
+                if l10==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==11:
+            for i in range(3):
+                if l11==r:
+                    z+=1
+            if z==1:
+                win=5
+        if a==12:
+            for i in range(3):
+                if l12==r:
+                    z+=1
+            if z==1:
+                win=5
+
+    if zak2>120 and zak2<130:#2:1
+        a=120-int(zak2)
+        if a==1:
+            for i in range(12):
+                if p12==r:
+                    z+=1
+            if z==1:
+                win=2
+        if a==2:
+            for i in range(12):
+                if d12==r:
+                    z+=1
+            if z==1:
+                win=2
+        if a==3:
+            for i in range(12):
+                if t12==r:
+                    z+=1
+            if z==1:
+                win=2
+
+    if zak2>180 and zak2<190:#1:1
+        a=180-int(zak2)
+        if a==1:
+            for i in range(18):
+                if p18==r:
+                    z+=1
+            if z==1:
+                win=1
+        if a==2:
+            for i in range(18):
+                if d18==r:
+                    z+=1
+            if z==1:
+                win=1
+
+    if zak2>220 and zak2<230:#17:1
+        a=220-int(zak2)
+    if zak2>330 and zak2<340:#11:1
+        a=330-int(zak2)
+    if zak2>440 and zak2<450:#8:1
+        a=440-int(zak2)
+    if zak2==5:
+        for i in range(5):
+            if t5 == r:
+                z += 1
+        if z == 1:
+            win = 1
+    if z==0:
+        win=0
+    return win
 
 def ruletka(cash):
     print("-----------------------------------")
